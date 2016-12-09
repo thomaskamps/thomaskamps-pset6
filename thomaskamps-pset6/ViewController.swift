8ed368d2c79c76026cf6c4f1250fce35baf07f93
thomaskamps <thomas@kemusic.nl>
2016-12-09 14:25:59 +0100

    Initial Commit

---
+++ thomaskamps-pset6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  thomaskamps-pset6
+//
+//  Created by Thomas Kamps on 09-12-16.
+//  Copyright © 2016 Thomas Kamps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
