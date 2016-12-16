4ec6977b317299d271f8551f6bc4237e9a5f83c2
thomaskamps <thomas@kemusic.nl>
2016-12-16 23:56:02 +0100

    FinalCommitNew

--- thomaskamps-pset6/AppDelegate.swift
+++ thomaskamps-pset6/AppDelegate.swift
@@ -43,14 +43,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
     
-    func application(application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool {
+    func application(_ application: UIApplication, shouldSaveApplicationState coder: NSCoder) -> Bool {
         return true
     }
     
-    func application(application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
+    func application(_ application: UIApplication, shouldRestoreApplicationState coder: NSCoder) -> Bool {
         return true
     }
-
 
 }
 
